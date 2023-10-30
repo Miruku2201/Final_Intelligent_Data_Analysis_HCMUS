3bf8ee1d1984d32c69d5ce7b031c9859f7b8afca
Miruku2201 <miruku2201@gmail.com>
2023-10-30 09:50:53 +0700

    commit before checkout

--- Crawl/GrabCrawling.py
+++ Crawl/GrabCrawling.py
@@ -7,13 +7,24 @@ desired_capabilities = {
     "appActivity": "com.grab.pax.newface.presentation.newface.NewFace",
 }
 
-driver = webdriver.Remote(
+
+# Generating the driver by appium.
+driver = webdriver.Remote(  
     "http://localhost:4723/wd/hub", desired_capabilities=desired_capabilities
 )
 
+## Waiting the android application to launch
 driver.implicitly_wait(15)
 
+## Sign in to the Grab app.
 driver.find_element(
     "xpath",
     "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.widget.TextView[2]",
 ).click()
+
+## Wait for launching authorities 
+driver.implicitly_wait(20)
+
+driver.find_element("id", "com.android.permissioncontroller:id/permission_allow_foreground_only_button").click()
+print("stop")
+
---
+++ assets/markdown/report.md
@@ -0,0 +1,24 @@
+---
+title: "Inteligent Data Analysis Application"
+title-size: 100pt
+subject: "Markdown"
+keywords: [Markdown, Example]
+# subtitle: "Topics: The Influence Of Weather On Life"
+lang: "vi"
+titlepage: true,
+titlepage-rule-color: "360049"
+titlepage-background: "assets/background/background1.pdf"
+titlepage-rule-height: 0
+book: true
+toc-own-page: true
+toc: true
+header-right: \today
+...
+
+# 1. Giới thiệu
+## 1.1 Thông tin đề tài.
+
+
+# 2. Cài đặt môi trường và framework
+# 3. Nội dung
+# 4. Tài liệu tham khảo
